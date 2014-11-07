bec1d714b0bf79e11a572de33a10900f92bf0c52
Eric GELOEN <geloen.eric@gmail.com>
2014-11-07 23:44:12 +0100

    [Encapsulation] Move everything from protected to private (except for entry point)

--- Tests/DependencyInjection/IvoryOrderedFormExtensionTest.php
+++ Tests/DependencyInjection/IvoryOrderedFormExtensionTest.php
@@ -22,7 +22,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class IvoryOrderedFormExtensionTest extends \PHPUnit_Framework_TestCase
 {
     /** @var \Symfony\Component\DependencyInjection\ContainerBuilder */
-    protected $container;
+    private $container;
 
     /**
      * {@inheritdoc}
