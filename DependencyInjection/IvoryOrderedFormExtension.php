7acf274ed85c8edf99a02abc834052fdc05722c5
GeLo <geloen.eric@gmail.com>
2015-12-12 19:42:38 +0100

    Make code base compatible with Symfony3

--- DependencyInjection/IvoryOrderedFormExtension.php
+++ DependencyInjection/IvoryOrderedFormExtension.php
@@ -15,6 +15,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Ivory Ordered Form extension.
@@ -30,5 +31,15 @@ class IvoryOrderedFormExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        if (Kernel::VERSION_ID < 20800) {
+            $container->getDefinition('ivory_ordered_form.form_extension')
+                ->clearTag($tag = 'form.type_extension')
+                ->addTag($tag, array('alias' => 'form'));
+
+            $container->getDefinition('ivory_ordered_form.button_extension')
+                ->clearTag($tag)
+                ->addTag($tag, array('alias' => 'button'));
+        }
     }
 }
